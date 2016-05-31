db5b2f6579a3d5a1b1e96c3b95080f3ba8b6ebff
Bransom2011 <bransom2011@gmail.com>
2016-05-31 05:54:40 -0400

    added userService Split Login Controller and profile controller from user controller refactored login services to connect to user services

---
+++ public/assignment/views/user/login.controller.client.js
@@ -0,0 +1,26 @@
+(function(){
+    angular
+        .module("WebAppMaker")
+        .controller("LoginController",LoginController);
+
+    function LoginController($location, UserService){
+
+        var vm = this;
+
+        vm.hello = "hello from view model";
+        vm.login = login;
+
+        function login (username, password){
+           var user = UserService.findUserByUsernameAndPassword(username, password);
+            if(user){
+                var id = user._id;
+                $location.url("/profile/" + id);
+            }
+            else {
+                vm.error = "User not found";
+            }
+        }
+
+        }
+
+})();
