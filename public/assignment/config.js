b402971269c47ac06f3a4a03d7957a26131d5e7f
Bransom2011 <bransom2011@gmail.com>
2016-05-31 04:04:21 -0400

    added profile page routing. created profile controller. used route params to parse path parameters. rendered current user info in profile view

--- public/assignment/config.js
+++ public/assignment/config.js
@@ -7,12 +7,19 @@
         $routeProvider
             .when("/login", {
                 templateUrl: "views/user/login.view.client.html",
-                controller: 'LoginController',
+                controller: "LoginController",
                 controllerAs:"model"
             })
 
             .when("/register", {
                 templateUrl: "views/user/register.view.client.html"
+            })
+
+            .when("/profile/:id", {
+                templateUrl: "views/user/profile.view.client.html",
+                controller: "ProfileController",
+                controllerAs: "model"
+
             })
         
 
